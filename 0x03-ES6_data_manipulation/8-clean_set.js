2fc01ed1d1241aa2dbc916e79132bfb608c13251
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-09 13:19:32 +0300

    fix:return a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(setArray, startString) {
   const finalString = [];
   if (startString && startString.length > 0) {
     setArray.forEach((element) => {
-      if (element.startsWith(startString)) {
+      if (typeof element === 'string' && element.startsWith(startString)) {
         finalString.push(element.replace(startString, ''));
       }
     });
