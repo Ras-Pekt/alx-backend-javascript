06d5132432446e06369d41536091a334312de711
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-12 12:38:27 +0300

    create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,7 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+  return {
+    allEmployees,
+    getNumberOfDepartments: () => Object.keys(allEmployees).length,
+  };
+}
