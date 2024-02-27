b6955c21bdfe06a088956be117bee034d585f24f
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-27 23:24:52 +0300

    express http server

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
-const app = require('express')();
+const express = require('express');
 const countStudents = require('./3-read_file_async');
-const port = 1245;
 
-app.get('/', (request, response) => response.send('Hello Holberton School!'));
-app.get('/students', async (req, response) => {
+const app = express();
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', async (req, res) => {
   const title = 'This is the list of our students\n';
   try {
     const data = await countStudents(process.argv[2]);
-    response.send(`${title}${data.join('\n')}`);
+    res.send(`${title}${data.join('\n')}`);
   } catch (error) {
-    response.send(`${title}${error.message}`);
+    res.send(`${title}${error.message}`);
   }
 });
-app.listen(port);
+app.listen(1245);
 module.exports = app;
