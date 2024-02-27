07897a7774b789d2a5dc64b69a77d511c44dd946
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-27 12:21:34 +0300

    read a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,29 +3,6 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  // try {
-  //   const contents = fs.readFileSync(path, 'utf-8');
-  //   const r = contents.trim().split('\n');
-
-  //   const dataObj = {};
-
-  //   console.log(`Number of students: ${r.length - 1}`);
-  //   for (let i = 1; i < r.length; i += 1) {
-  //     const key = r[i].split(',')[3];
-  //     const value = r[i].split(',')[0];
-  //     if (key in dataObj) {
-  //       dataObj[key].push(value);
-  //     } else {
-  //       dataObj[key] = [value];
-  //     }
-  //   }
-  //   for (const [key, value] of Object.entries(dataObj)) {
-  //     const numStudents = value.length;
-  //     console.log(`Number of students in ${key}: ${numStudents}. List: ${value.join(', ')}`);
-  //   }
-  // } catch (err) {
-  //   console.log('Cannot load the database');
-  // }
   if (fs.existsSync(path)) {
     try {
       const data = fs.readFileSync(path, 'utf8');
