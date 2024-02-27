8be5bc317f1060b64fd34a5f206d3967b220afd9
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-27 12:06:25 +0300

    use process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,3 @@
-#!/usr/bin/node
-
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -7,12 +5,13 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?')
+console.log('Welcome to Holberton School, what is your name?');
 rl.on('line', (input) => {
-  console.log(`Your name is ${input}`);
+  console.log(`Your name is: ${input}`);
   rl.close();
 });
 
 rl.on('close', () => {
   console.log('This important software is now closing');
+  rl.close();
 });
