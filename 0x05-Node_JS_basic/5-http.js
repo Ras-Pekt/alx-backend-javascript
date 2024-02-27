24262dc3ada67d7fd3976c28742cb29a47c1b612
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-27 23:29:11 +0300

    node http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,22 +1,63 @@
 #!/usr/bin/node
 
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+      reject(Error('Cannot load the database'));
+    } else {
+      const lines = data.split('\n');
+      let numStudents = 0;
+      const courseStudents = {};
+      for (let i = 1; i < lines.length; i += 1) {
+        if (lines[i].trim()) {
+          const line = lines[i].split(',');
+          const firstName = line[0];
+          const field = line[line.length - 1];
+          if (field in courseStudents) {
+            courseStudents[field].push(firstName);
+          } else {
+            courseStudents[field] = [firstName];
+          }
+          numStudents += 1;
+        }
+      }
+      const messages = [
+        `Number of students: ${numStudents}`,
+      ];
+      console.log();
+      for (const [field, students] of Object.entries(courseStudents)) {
+        messages.push(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      }
+      resolve(messages);
     }
-  }
-  res.end();
+  });
 });
+
+const app = http.createServer();
+
+app.on('request', async (req, res) => {
+  switch (req.url) {
+    case '/students':
+      res.write('This is the list of our students\n');
+      countStudents(process.argv[2])
+        .then((messages) => {
+          const final = `${messages.join('\n')}`;
+          res.statusCode = 200;
+          res.end(final);
+        }).catch((err) => {
+          const final = `${err.message}`;
+          res.statusCode = 404;
+          res.end(final);
+        });
+      break;
+    default:
+      res.end('Hello Holberton School!');
+  }
+});
+
 app.listen(1245);
+
 module.exports = app;
